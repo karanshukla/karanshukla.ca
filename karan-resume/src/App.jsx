756b9458017bb19e51953ea7986252310c44eb1a
Karz <karan_shukla@live.com>
2024-07-22 19:10:18 -0400

    add material Ui as a test

--- karan-resume/src/App.jsx
+++ karan-resume/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div>
-      <h1>Karan Shukla</h1>
-    </div>
+      <div>
+          <h1>Karan Shukla</h1>
+          <p>Software Engineer</p>
+          <Button variant="contained">Hello world</Button>
+      </div>
   );
 }
 
---
+++ karan-resume/src/Helmet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Helmet() {
+  return (
+    <><head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+    </head></>
+  );
+}
+
+export default Helmet;
