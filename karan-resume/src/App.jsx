4cd17d20da957c10771a38307ca041ad4c2f809a
Karz <karan_shukla@live.com>
2024-07-23 14:20:51 -0400

    remove npm and upgrade to yarn

--- karan-resume/src/App.jsx
+++ karan-resume/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Container , Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <div style={{ display: 'flex' }}>
-        <div style={{ flex: '0 0 20%', marginRight: '1rem' }}>
-          <Typography variant="h1">Karan Shukla</Typography>
-          <Typography variant="subtitle1">Software Engineer</Typography>
-        </div>
+    <Container>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <Typography variant="h1">Karan Shukla</Typography>
+        <Typography variant="subtitle1">Software Engineer</Typography>
         <div style={{ flex: '1' }}>
           {/* Content */}
         </div>
