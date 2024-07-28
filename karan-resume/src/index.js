e66f8ac0405563953556cbf0f58080747ee4de50
Karz <karan_shukla@live.com>
2024-07-28 14:28:48 -0400

    move to a better folder structure

--- karan-resume/src/index.js
+++ karan-resume/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Helmet from './Helmet';
+import Helmet from './components/Helmet';
+import App from './components/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
