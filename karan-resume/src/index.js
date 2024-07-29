3bf6ce89339ded7f3f8fa6436a0686165c8a1e07
Karz <karan_shukla@live.com>
2024-07-29 12:58:33 -0400

    move and fix asher pic

--- karan-resume/src/index.js
+++ karan-resume/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Helmet from './components/Helmet';
-import Home from './components/Home.js';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
