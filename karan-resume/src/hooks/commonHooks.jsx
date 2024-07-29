ee5db79174b9498be4b0194149d99ab715df1aea
Karz <karan_shukla@live.com>
2024-07-29 11:58:18 -0400

    default to dark mode

--- karan-resume/src/hooks/commonHooks.jsx
+++ karan-resume/src/hooks/commonHooks.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { createTheme } from '@mui/material/styles';
 
 export const useTheme = () => {
-    const [themeMode, setThemeMode] = useState(localStorage.getItem('theme') || 'light');
+    const [themeMode, setThemeMode] = useState(localStorage.getItem('theme') || 'dark');
 
     const toggleTheme = () => {
-      const newThemeMode = themeMode === 'light' ? 'dark' : 'light';
+      const newThemeMode = themeMode === 'dark' ? 'light' : 'dark';
       setThemeMode(newThemeMode);
       localStorage.setItem('theme', newThemeMode);
     };
